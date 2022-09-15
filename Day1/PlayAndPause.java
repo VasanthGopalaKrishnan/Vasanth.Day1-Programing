739c7d09a59affc7b27d670f4b3a89c383fcc4c0
Vasanth.G <113467011+VasanthGopalaKrishnan@users.noreply.github.com>
2022-09-15 23:07:56 +0530

    Update PlayAndPause.java

--- Day1/PlayAndPause.java
+++ Day1/PlayAndPause.java
@@ -1,5 +1,8 @@
 package Day1;
 
+// Interface is used to hiding the implements
+// we cannot create object if we declare a class as interface
+
 public interface PlayAndPause {
 
     void playMusic();
