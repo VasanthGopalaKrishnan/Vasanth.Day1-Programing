412af8c0d8359e38400fb56158e8a2a70e6e28a3
Vasanth.G <113467011+VasanthGopalaKrishnan@users.noreply.github.com>
2022-09-15 23:20:42 +0530

    Update OrderDelivered.java

--- Day2/OrderDelivered.java
+++ Day2/OrderDelivered.java
@@ -2,6 +2,10 @@ package Day2;
 
 import java.util.Scanner;
 
+
+// Whenever execution starts it will check for static block if static block is present, before execution static block is executed and then the execution will continue.
+// The super keyword refers to superclass (parent) objects. It is used to call superclass methods, and to access the superclass constructor.
+
 public class OrderDelivered extends TShirtOrders{
 
 
