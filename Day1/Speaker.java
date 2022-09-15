d7cbff9db73657c7e444e85508ea9747d7ee1be6
Vasanth.G <113467011+VasanthGopalaKrishnan@users.noreply.github.com>
2022-09-15 23:15:11 +0530

    Update Speaker.java

--- Day1/Speaker.java
+++ Day1/Speaker.java
@@ -1,5 +1,10 @@
 package Day1;
 
+// overriding different class,same method name,but different argument
+// By using the overriding method we can override the methods of parent class in child class
+// Implements keyword is used to implement the interfaces
+// By using implements keyword we can access more than one class properties in child class
+
 public class Speaker implements Power,Volume,PlayAndPause{
 
     @Override
