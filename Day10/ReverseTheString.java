85bb63f64d2ee2517681e7439b86304a1aeb979b
Vasanth.G <113467011+VasanthGopalaKrishnan@users.noreply.github.com>
2022-09-26 23:30:26 +0530

    Add files via upload

---
+++ Day10/ReverseTheString.java
@@ -0,0 +1,24 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class ReverseTheString {
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            String A=sc.next();
+            String rev="";
+            for(int i=A.length()-1;i>=0;i--)
+            {
+                rev = rev+A.charAt(i);
+            }
+            if(rev.equals(A))
+                System.out.print("Yes");
+            else
+                System.out.print("No");
+
+        }
+
+    }
+
