49357ff060cb7c4d0485051315f44f6e9d487f00
Vasanth.G <113467011+VasanthGopalaKrishnan@users.noreply.github.com>
2022-09-30 10:30:48 +0530

    Add files via upload

---
+++ Day13/StringSplit.java
@@ -0,0 +1,22 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+public class StringSplit {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        s = s.trim();
+        if (s.length() == 0) {
+            System.out.println(0);
+        } else {
+            String[] strings = s.split("['!?,._@ ]+");
+            System.out.println(strings.length);
+            for (String str : strings)
+                System.out.println(str);
+        }
+        scan.close();
+    }
+
+}
